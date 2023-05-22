07121e2946bb171a0653d4b45837244322fa990c
deepak <dkp041297@gmail.com>
2023-05-22 11:18:29 +0530

    day1

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+    <h1>Hye I am  Deepak </h1>
+    <p>Today i create  a netFlix App</p>
+    </main>
+  )
+}
